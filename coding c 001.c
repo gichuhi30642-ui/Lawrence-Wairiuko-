883cff1a70b2205044321ba9ad0b08ceddf9e56f
gichuhi30642-ui <gichuhi.30642@students.kyu.ac.ke>
2026-09-17 18:10:35 +0300

    Add files via upload

---
+++ coding c 001.c
@@ -0,0 +1,25 @@
+//single line comment
+//program to display hello word
+
+/*
+(multiline comment)
+Name:Lawrence
+  Reg No : CT100/G/30642/26
+  Description:Hello world printf("");int
+  Date: 12/9/2026
+  version 1
+  */
+ 
+ //pre-processor directive
+  #include<stdio.h> // scanf(),printf("");
+   #include<stdio.h> //scanf(),printf("");
+   
+   #define PI 3.142
+   
+   int main () {
+   printf("Hello world.");
+   printf("welcome to c programming. \n");
+   printf("remember to come\r");
+   
+   return 0;//excution successful
+}
