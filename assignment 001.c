343013c3a4ebf214c0cfca772b8f19d2c3d4e891
gichuhi30642-ui <gichuhi.30642@students.kyu.ac.ke>
2026-09-17 18:32:41 +0300

    Upload assignment

---
+++ assignment 001.c
@@ -0,0 +1,29 @@
+//single line comment
+//program to display year one units
+
+/*
+(multiline comment)
+Name:Lawrence
+  Reg No : CT100/G/30642/26
+  Description:Hello world printf("");int
+  Date: 12/9/2026
+  version 1
+  */
+ 
+ //pre-processor directive
+  #include<stdio.h> // scanf(),printf("");
+   #include<stdio.h> //scanf(),printf("");
+   
+   #define PI 3.142
+   
+   int main () {
+   printf("mathematics for science.");
+   printf("essentials of accounting. \n");
+   printf("introduction to computer application\r");
+   printf("introduction to programming and algorithms");
+   printf("communication and information literacy skills");
+   printf("analogue electronics");
+   printf("Fundamentals of Digital Technology and Atificial Inteligence");
+   return 0;//excution successful
+
+}
